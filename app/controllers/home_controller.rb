a46f3e5e7ae99534cbac02477f257eaed6f1c419
Jiří Vrba <jiri@vrba.dev>
2020-11-09 00:37:18 +0100

    Home controller logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    redirect to: 'home#dashboard' if user_signed_in?
-
-    render html: 'Index'
+    redirect_to dashboard_path if user_signed_in?
   end
 
   def dashboard
