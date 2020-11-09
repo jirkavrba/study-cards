c4301917e445c15e00f2189d032e4a546ff14dbd
Jiří Vrba <jiri@vrba.dev>
2020-11-09 09:39:43 +0100

    Basic dashboard data bindings

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @collections = current_user.collections.recent
+    @collections = current_user.collections
+    @recent = @collections.recent
   end
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,5 +2,5 @@ class Collection < ApplicationRecord
   belongs_to :user
   has_many :cards, dependent: :destroy
 
-  scope :recent, -> { order(last_opened_at: :desc).take(5) }
+  scope :recent, -> { order(last_opened_at: :desc).first(5) }
 end
